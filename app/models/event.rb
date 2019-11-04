23f3a5ae4e2ccd2c3435037ed2ee2fc9dbc4bf16
Fanny Jestin <f.jestin@tbs-education.org>
2019-11-04 16:43:07 +0100

    modif

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,36 +3,26 @@ class Event < ApplicationRecord
 	has_many :users , through: :attendances
 	belongs_to :admin, class_name: "User"
 
-	validates :start_date,
-	presence: :true 
+	validates :start_date, presence: true 
 
-	validates :duration,
-	presence: :true,
-	:numericality => { :greater_than_or_equal_to => 0 },
+	validates :duration, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+
 	validate :multiple_of_5
 #le validate des méthodes est au singulier
 
+	validates :title, presence: true, length: {minimum: 5, maximum: 140}
 
-	validates :title,
-	presence: :true,
-	length: {minimum: 5, maximum: 140}
+	validates :description, presence: true, length: {minimum: 20, maximum: 1000}
 
-	validates :description,
-	presence: :true,
-	length: {minimum: 20, maximum: 1000}
+	validates :price, presence: true,
+    validates_inclusion_of :price, :in => 1..1000
 
-	validates :price,
-	presence: :true,
-	:numericality => { :greater_than_or_equal_to => 1 },
-	:numericality => { :less_than_or_equal_to => 1000 }
-
-	validates :location,
-	presence: :true
+	validates :location, presence: true
 
 	def multiple_of_5
-		if (self.duration % 5) != 0
-			self.errors[:base] << "Number must be divisible by 5!"
-		end 
+      if (self.duration % 5) != 0
+      self.errors[:base] << "Number must be divisible by 5!"
+      end
 	end
 
 end
